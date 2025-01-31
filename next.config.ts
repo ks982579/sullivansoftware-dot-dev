6b8313485bc738d0bce711ea3386942389b04d94
Kevin Sullivan <kevin.sullivan@sullivansoftware.dev>
2025-01-31 12:42:35 +0000

    Small Updates while looking into TS

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // images: {
+  //   remotePatterns: {
+  //     protocol: "https",
+  //     hostname: "placeholder.jp",
+  //   }
+  // }
 };
 
 export default nextConfig;
